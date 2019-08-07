3026239ab99adc120f8c072f8487a20dab11cd20
Claire Barela <claire.barela@gmail.com>
2019-08-07 21:40:52 +0800

    optimize imports

--- netmesh_web/views/agents.py
+++ netmesh_web/views/agents.py
@@ -5,12 +5,11 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from django.utils.translation import ugettext as _
-from django.urls import reverse_lazy
 
 from netmesh_api.models import AgentProfile
-from django.shortcuts import get_object_or_404
 
 
 class UserForm(forms.ModelForm):
