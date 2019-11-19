000de2eb391a52be361988c5caf470e5df6a3007
Claire Barela <claire.barela@gmail.com>
2019-11-19 13:42:30 +0800

    add filter/search option in results

--- netmesh_web/views/tests.py
+++ netmesh_web/views/tests.py
@@ -1,9 +1,13 @@
+from django.contrib import messages as alerts
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
+from django.utils.translation import ugettext as _
 
 from netmesh_api.models import DataPoint
 from netmesh_api.models import Test
+from netmesh_web.forms import SearchForm
 
 
 def test_detail(request, id, template_name='tests/detail.html'):
@@ -18,12 +22,41 @@ def test_detail(request, id, template_name='tests/detail.html'):
 
 
 def test_list(request, template_name='tests/list.html'):
-    test_lst = Test.objects.all().order_by('-date_created')
+    context = {}
+    if 'search' in request.GET:
+        test_lst = Test.objects.all().order_by('-date_created')
+        for term in request.GET['search'].split():
+            test_lst = test_lst.filter(Q(test_type__icontains=term) |
+                                       Q(mode__icontains=term) |
+                                       Q(ip_address__ip_address__icontains=term) |
+                                       Q(ip_address__isp__icontains=term) |
+                                       Q(agent__user__username__icontains=term)
+                                       )
+        context['search'] = True
+        alerts.info(request,
+                    _("You've searched for: '%s'") % request.GET['search'])
+    else:
+        test_lst = Test.objects.all().order_by('-date_created')
+
     paginator = Paginator(test_lst, 15)
     page = request.GET.get('page')
-    tests = paginator.get_page(page)
+
+    is_paginated = False
+    if paginator.num_pages > 1:
+        is_paginated = True
+
+    try:
+        tests = paginator.get_page(page)
+    except PageNotAnInteger:
+        tests = paginator.get_page(1)
+    except EmptyPage:
+        tests = paginator.get_page(paginator.num_pages)
+
+    form = SearchForm(form_action='tests')
     context = {
-        'tests': tests
+        'tests': tests,
+        'form': form,
+        'is_paginated': is_paginated
     }
     return render(request, template_name, context=context)
 
